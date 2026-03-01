86e2d3d6c3947bcd467a250b6e0d2164b2125d68
durgaprasaduniq <t.m.durgaprasad165@gmail.com>
2026-03-01 11:44:39 +0530

    Update ServletInitializer.java

--- project/demo/src/main/java/com/example/demo/ServletInitializer.java
+++ project/demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -11,3 +11,16 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	}
 
 }
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Deployment Successful 🚀";
+    }
+}
